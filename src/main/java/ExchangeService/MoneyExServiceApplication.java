18fcb963d70c9c2e48b6242c7e66a28949a35cd6
Nazar Kryvicko <Nazikkryvycko@gmail.com>
2023-07-31 12:27:59 +0300

    Application StartPoint

---
+++ src/main/java/ExchangeService/MoneyExServiceApplication.java
@@ -0,0 +1,13 @@
+package ExchangeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyExServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyExServiceApplication.class, args);
+    }
+
+}
